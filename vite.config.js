d5b77ddfaafd6246906d61a6d0d1e774726ec2a2
programmerrm <programmerwebrm@gmail.com>
2025-11-08 22:32:40 +0600

    demo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
+  build: { 
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'about/about.html'),
+        service: resolve(__dirname, 'service/service.html')
+      },
+    },
+  },
 })
